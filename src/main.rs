ac9942a5d9e57dd24195f4ede5ae49813291a2aa
Zubbbz <37497090+Zubbbz@users.noreply.github.com>
2024-06-01 05:14:11 -0400

    add cli arguments, improve api

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,14 @@ struct Password {
 }
 
 impl Password {
-	pub fn new(length: u8, charset: String) -> Self {
-		Self { length, charset }
+	pub fn new(length: Option<u8>, charset: String) -> Self {
+		match length {
+			Some(len) => Self {
+				length: len,
+				charset,
+			},
+			None => Self { length: 8, charset },
+		}
 	}
 
 	pub fn generate(&self) -> String {
@@ -23,25 +29,78 @@ impl Password {
 	}
 }
 
+struct Charset {
+	alphabet_lower: bool,
+	alphabet_upper: bool,
+	numbers: bool,
+	symbols: bool,
+}
+
+impl Charset {
+	pub fn default() -> Self {
+		Self {
+			alphabet_lower: true,
+			alphabet_upper: true,
+			numbers: true,
+			symbols: false,
+		}
+	}
+
+	pub fn get(&self) -> String {
+		let mut set: String = "".to_owned();
+		if self.alphabet_lower == true {
+			set.push_str("abcdefghijklmnopqrstuvwxyz")
+		}
+		if self.alphabet_upper == true {
+			set.push_str("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		}
+		if self.numbers == true {
+			set.push_str("1234567890");
+		}
+		if self.symbols == true {
+			set.push_str("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+		}
+		set
+	}
+}
+
+/// A simple program to generate passwords
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-	// Length of pass
+	// ? Default values are done elsewhere, currently not using defaults for the args, as option type doesnt implement display...
+	/// Length of password
 	#[arg(short, long)]
-	length: u8,
+	length: Option<u8>,
+
+	/// Enable lowercase letters
+	#[arg(long)]
+	lower: Option<bool>,
+
+	/// Enable Uppercase letters
+	#[arg(long)]
+	upper: Option<bool>,
+
+	/// Enable numbers
+	#[arg(long)]
+	numbers: Option<bool>,
+
+	/// Enable symbols
+	#[arg(long)]
+	symbols: Option<bool>,
 }
 
 fn main() {
-	let charset_alphabet_lower: &str = "abcdefghijklmnopqrstuvwxyz";
-	let charset_alphabet_upper: &str = &charset_alphabet_lower.to_uppercase();
-	let charset_numbers: &str = "1234567890";
-
 	let args = Args::parse();
-	let gen_charset: String = format!(
-		"{}{}{}",
-		charset_alphabet_lower, charset_alphabet_upper, charset_numbers
-	);
-	let pw = Password::new(args.length, gen_charset).generate();
+
+	let charset = Charset::get(&Charset {
+		alphabet_lower: args.lower.unwrap_or(Charset::default().alphabet_lower),
+		alphabet_upper: args.upper.unwrap_or(Charset::default().alphabet_upper),
+		numbers: args.numbers.unwrap_or(Charset::default().numbers),
+		symbols: args.symbols.unwrap_or(Charset::default().symbols),
+	});
+
+	let pw = Password::new(args.length, charset).generate();
 	println!("Password: {}\nlength: {}", pw, pw.len());
 }
 
